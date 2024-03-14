f52e10bf0828610deff7b087e30fb795a9a75d2d
Abhay Rana <113694151+Abhay-Rana-18@users.noreply.github.com>
2024-03-14 09:07:30 +0530

    Create 14th March.java
    930. Binary Subarrays With Sum
    
    https://leetcode.com/problems/binary-subarrays-with-sum/description/?envType=daily-question&envId=2024-03-14

---
+++ 14th March.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int solve(int []nums, int k){
+        if(k<0){
+            return 0;
+        }
+        int j=0;
+        int i=0;
+        int sum=0;
+        int ans=0;
+        while(i<nums.length){
+            sum+=nums[i];
+            while(sum>k&&j<nums.length){
+            sum-=nums[j];
+                j++;
+            }
+            if(sum<=k){
+                ans+=i-j+1;
+                }
+            i++;
+        }
+        return ans;
+    }
+    public int numSubarraysWithSum(int[] nums, int goal) {
+       int x=solve(nums,goal);
+       int y=solve(nums,goal-1);
+       return x-y;
+    }
+}
