451e5024ddd29ad6d2643d7422e24c7476150db0
Abhay Rana <113694151+Abhay-Rana-18@users.noreply.github.com>
2024-04-19 12:48:51 +0530

    Create 19th April.java
    200. Number of Islands
    
    https://leetcode.com/problems/number-of-islands/description/?envType=daily-question&envId=2024-04-19

---
+++ 19th April.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        int islands = 0;
+        int r = grid.length;
+        int c = grid[0].length;
+
+        for (int i=0; i<r; i++) {
+            for (int j=0; j<c; j++) {
+                if (grid[i][j] == '1') {
+                    islands++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+
+        return islands;
+    }
+
+    private void dfs(char[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != '1') {
+            return;
+        }
+        
+        grid[i][j] = '0'; // mark as visited
+        dfs(grid, i + 1, j); // down
+        dfs(grid, i - 1, j); // up
+        dfs(grid, i, j + 1); // right
+        dfs(grid, i, j - 1); // left
+    }
+}
